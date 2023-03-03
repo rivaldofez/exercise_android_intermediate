9e9bb7dcaa706cf7484b9e693b27beb4a04f0640
rivaldofez <rivaldofez@gmail.com>
2023-03-04 00:13:56 +0700

    create face layout using canvas

--- LikesApp/app/src/main/java/com/rivaldofez/likesapp/MainActivity.kt
+++ LikesApp/app/src/main/java/com/rivaldofez/likesapp/MainActivity.kt
@@ -1,16 +1,48 @@
 package com.rivaldofez.likesapp
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
 import com.rivaldofez.likesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val mBitmap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
+    private val mCanvas = Canvas(mBitmap)
+    private val mPaint = Paint()
+
+    private val halfOfWidth = (mBitmap.width/2).toFloat()
+    private val halfOfHeight = (mBitmap.height/2).toFloat()
+
+    private val left = 150F
+    private val top = 250F
+    private val right = mBitmap.width - left
+    private val bottom = mBitmap.height.toFloat() - 150F
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.imageView.setImageBitmap(mBitmap)
+
+
+
+        showFace()
+    }
+
+    private fun showFace() {
+        val face = RectF(left, top, right, bottom)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.yellow_left_skin, null)
+        mCanvas.drawArc(face, 90F, 180F, false, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.yellow_right_skin, null)
+        mCanvas.drawArc(face, 270F, 180F, false, mPaint)
     }
 }
