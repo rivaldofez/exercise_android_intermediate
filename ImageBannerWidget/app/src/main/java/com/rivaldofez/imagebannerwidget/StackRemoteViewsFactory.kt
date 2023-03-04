f4cfbdc25d866cb83e52a35df0edb491bb4fb47b
rivaldofez <rivaldofez@gmail.com>
2023-03-04 13:15:59 +0700

    add StackWidgetFactory

---
+++ ImageBannerWidget/app/src/main/java/com/rivaldofez/imagebannerwidget/StackRemoteViewsFactory.kt
@@ -0,0 +1,63 @@
+package com.rivaldofez.imagebannerwidget
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.widget.RemoteViews
+import android.widget.RemoteViewsService
+import androidx.core.os.bundleOf
+
+internal class StackRemoteViewsFactory(private val mContext: Context) : RemoteViewsService.RemoteViewsFactory {
+
+    private val mWidgetItems = ArrayList<Bitmap>()
+
+    override fun onDataSetChanged() {
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.resources, R.drawable.darth_vader))
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.resources, R.drawable.star_wars_logo))
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.resources, R.drawable.storm_trooper))
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.resources, R.drawable.starwars))
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.resources, R.drawable.falcon))
+
+    }
+
+    override fun onCreate() {
+
+    }
+
+    override fun onDestroy() {
+
+    }
+
+    override fun getCount(): Int = mWidgetItems.size
+
+    override fun getViewAt(position: Int): RemoteViews {
+        val rv = RemoteViews(mContext.packageName, R.layout.widget_item)
+        rv.setImageViewBitmap(R.id.imageView, mWidgetItems[position])
+        val extras = bundleOf(
+            ImagesBannerWidget.EXTRA_ITEM to position
+        )
+        val fillInIntent = Intent()
+        fillInIntent.putExtras(extras)
+        rv.setOnClickFillInIntent(R.id.imageView, fillInIntent)
+        return rv
+    }
+
+    override fun getLoadingView(): RemoteViews {
+
+    }
+
+    override fun getViewTypeCount(): Int {
+
+    }
+
+    override fun getItemId(p0: Int): Long {
+
+    }
+
+    override fun hasStableIds(): Boolean {
+
+    }
+
+
+}
