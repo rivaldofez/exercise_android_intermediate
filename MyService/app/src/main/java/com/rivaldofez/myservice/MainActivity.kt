d537f98e1be6ce562d4e80f68c0b89d75d36e442
rivaldofez <rivaldofez@gmail.com>
2023-03-10 16:38:54 +0700

    implement background service

--- MyService/app/src/main/java/com/rivaldofez/myservice/MainActivity.kt
+++ MyService/app/src/main/java/com/rivaldofez/myservice/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.rivaldofez.myservice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rivaldofez.myservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val serviceIntent = Intent(this, MyBackgroundService::class.java)
+        binding.btnStartBackgroundService.setOnClickListener {
+            startService(serviceIntent)
+        }
+        binding.btnStopBackgroundService.setOnClickListener {
+            stopService(serviceIntent)
+        }
     }
 }
