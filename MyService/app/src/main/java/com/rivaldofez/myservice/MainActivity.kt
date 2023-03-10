efffe2bb77156a613e3f8b8ce17a723ce51f4ce7
rivaldofez <rivaldofez@gmail.com>
2023-03-10 18:24:45 +0700

    add foreground service

--- MyService/app/src/main/java/com/rivaldofez/myservice/MainActivity.kt
+++ MyService/app/src/main/java/com/rivaldofez/myservice/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rivaldofez.myservice
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rivaldofez.myservice.databinding.ActivityMainBinding
@@ -19,6 +20,18 @@ class MainActivity : AppCompatActivity() {
         }
         binding.btnStopBackgroundService.setOnClickListener {
             stopService(serviceIntent)
+        }
+
+        val foregroundServiceIntent = Intent(this, MyForegroundService::class.java)
+        binding.btnStartForegroundService.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= 26) {
+                startForegroundService(foregroundServiceIntent)
+            } else {
+                startService(foregroundServiceIntent)
+            }
+        }
+        binding.btnStopForegroundService.setOnClickListener {
+            stopService(foregroundServiceIntent)
         }
     }
 }
