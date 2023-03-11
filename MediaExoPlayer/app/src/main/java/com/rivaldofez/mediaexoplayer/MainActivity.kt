d865640ec4abb67bed1d8dae69f5c07dbe716085
rivaldofez <rivaldofez@gmail.com>
2023-03-11 12:49:41 +0700

    implement exoplayer with video and audio

--- MediaExoPlayer/app/src/main/java/com/rivaldofez/mediaexoplayer/MainActivity.kt
+++ MediaExoPlayer/app/src/main/java/com/rivaldofez/mediaexoplayer/MainActivity.kt
@@ -2,10 +2,31 @@ package com.rivaldofez.mediaexoplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.rivaldofez.mediaexoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val URL_VIDEO_DICODING = "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"
+        const val URL_AUDIO = "https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3"
+    }
+
+    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
+
+        val player = ExoPlayer.Builder(this).build()
+        viewBinding.videoView.player = player
+
+        val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
+        val anotherMediaItem = MediaItem.fromUri(URL_AUDIO)
+        player.setMediaItem(mediaItem)
+        player.addMediaItem(anotherMediaItem)
+        player.prepare()
     }
 }
