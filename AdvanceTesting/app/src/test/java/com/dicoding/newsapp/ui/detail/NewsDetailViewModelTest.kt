1977c5c0f49c78a420cfaf4db1a5280b062f255a
rivaldofez <rivaldofez@gmail.com>
2023-04-01 22:21:54 +0700

    implement test with coroutines

---
+++ AdvanceTesting/app/src/test/java/com/dicoding/newsapp/ui/detail/NewsDetailViewModelTest.kt
@@ -0,0 +1,59 @@
+package com.dicoding.newsapp.ui.detail
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.dicoding.newsapp.data.NewsRepository
+import org.junit.Assert.*
+
+import com.dicoding.newsapp.data.Result
+import com.dicoding.newsapp.utils.DataDummy
+import com.dicoding.newsapp.utils.getOrAwaitValue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.*
+import org.junit.*
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class NewsDetailViewModelTest{
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+    @Mock
+    private lateinit var newsRepository: NewsRepository
+    private lateinit var newsDetailViewModel: NewsDetailViewModel
+    private val dummyDetailNews = DataDummy.generateDummyNewsEntity()[0]
+    @Before
+    fun setUp() {
+        newsDetailViewModel = NewsDetailViewModel(newsRepository)
+        newsDetailViewModel.setNewsData(dummyDetailNews)
+    }
+
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+
+    @Before
+    fun setupDispatcher() {
+        Dispatchers.setMain(testDispatcher)
+    }
+    @After
+    fun tearDownDispatcher() {
+        Dispatchers.resetMain()
+    }
+
+
+    @Test
+    fun `when bookmarkStatus false Should call saveNews`() = runTest {
+        val expectedBoolean = MutableLiveData<Boolean>()
+        expectedBoolean.value = false
+        `when`(newsRepository.isNewsBookmarked(dummyDetailNews.title)).thenReturn(expectedBoolean)
+        newsDetailViewModel.bookmarkStatus.getOrAwaitValue()
+        newsDetailViewModel.changeBookmark(dummyDetailNews)
+        Mockito.verify(newsRepository).saveNews(dummyDetailNews)
+    }
+}
+
+
+
