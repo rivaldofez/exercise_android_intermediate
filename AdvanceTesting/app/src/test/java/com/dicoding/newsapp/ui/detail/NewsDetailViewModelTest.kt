18e17c441b2333ca628c8187db7c2688636a7752
rivaldofez <rivaldofez@gmail.com>
2023-04-01 22:25:32 +0700

    add test case for bookmarked item

--- AdvanceTesting/app/src/test/java/com/dicoding/newsapp/ui/detail/NewsDetailViewModelTest.kt
+++ AdvanceTesting/app/src/test/java/com/dicoding/newsapp/ui/detail/NewsDetailViewModelTest.kt
@@ -7,8 +7,10 @@ import org.junit.Assert.*
 
 import com.dicoding.newsapp.data.Result
 import com.dicoding.newsapp.utils.DataDummy
+import com.dicoding.newsapp.utils.MainDispatcherRule
 import com.dicoding.newsapp.utils.getOrAwaitValue
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.*
 import org.junit.*
@@ -18,31 +20,26 @@ import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 
+@ExperimentalCoroutinesApi
 @RunWith(MockitoJUnitRunner::class)
 class NewsDetailViewModelTest{
+
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
+
     @Mock
     private lateinit var newsRepository: NewsRepository
     private lateinit var newsDetailViewModel: NewsDetailViewModel
     private val dummyDetailNews = DataDummy.generateDummyNewsEntity()[0]
+
     @Before
     fun setUp() {
         newsDetailViewModel = NewsDetailViewModel(newsRepository)
         newsDetailViewModel.setNewsData(dummyDetailNews)
     }
 
-    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
-
-    @Before
-    fun setupDispatcher() {
-        Dispatchers.setMain(testDispatcher)
-    }
-    @After
-    fun tearDownDispatcher() {
-        Dispatchers.resetMain()
-    }
-
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
 
     @Test
     fun `when bookmarkStatus false Should call saveNews`() = runTest {
@@ -53,6 +50,18 @@ class NewsDetailViewModelTest{
         newsDetailViewModel.changeBookmark(dummyDetailNews)
         Mockito.verify(newsRepository).saveNews(dummyDetailNews)
     }
+
+
+    @Test
+    fun `when bookmarkStatus true Should call deleteNews`() = runTest {
+        val expectedBoolean = MutableLiveData<Boolean>()
+        expectedBoolean.value = true
+        `when`(newsRepository.isNewsBookmarked(dummyDetailNews.title)).thenReturn(expectedBoolean)
+        newsDetailViewModel.bookmarkStatus.getOrAwaitValue()
+        newsDetailViewModel.changeBookmark(dummyDetailNews)
+        Mockito.verify(newsRepository).deleteNews(dummyDetailNews.title)
+    }
+
 }
 
 
