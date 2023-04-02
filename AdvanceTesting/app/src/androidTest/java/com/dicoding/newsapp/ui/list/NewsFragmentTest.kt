fc4e2e39348ea4233b1aee757c4ccb91ae1a5bee
rivaldofez <rivaldofez@gmail.com>
2023-04-03 01:16:29 +0700

    implement test fragment

--- AdvanceTesting/app/src/androidTest/java/com/dicoding/newsapp/ui/list/NewsFragmentTest.kt
+++ AdvanceTesting/app/src/androidTest/java/com/dicoding/newsapp/ui/list/NewsFragmentTest.kt
@@ -2,12 +2,16 @@ package com.dicoding.newsapp.ui.list
 
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.dicoding.newsapp.R
 import com.dicoding.newsapp.data.remote.retrofit.ApiConfig
+import com.dicoding.newsapp.utils.JsonConverter
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
@@ -35,9 +39,37 @@ class NewsFragmentTest {
         val bundle = Bundle()
         bundle.putString(NewsFragment.ARG_TAB, NewsFragment.TAB_NEWS)
         launchFragmentInContainer<NewsFragment>(bundle, R.style.Theme_News)
+
+        val mockResponse = MockResponse()
+            .setResponseCode(200)
+            .setBody(JsonConverter.readStringFromFile("success_response.json"))
+        mockWebServer.enqueue(mockResponse)
+
         onView(withId(R.id.rv_news))
             .check(matches(isDisplayed()))
-        //check data is match
+        onView(withText("Inti Bumi Mendingin Lebih Cepat, Pertanda Apa? - detikInet"))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.rv_news))
+            .perform(
+                RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(
+                    hasDescendant(withText("Perjalanan Luar Angkasa Sebabkan Anemia - CNN Indonesia"))
+                )
+            )
     }
+
     //check if failed load data
+    @Test
+    fun getHeadlineNews_Error() {
+        val bundle = Bundle()
+        bundle.putString(NewsFragment.ARG_TAB, NewsFragment.TAB_NEWS)
+        launchFragmentInContainer<NewsFragment>(bundle, R.style.Theme_News)
+        val mockResponse = MockResponse()
+            .setResponseCode(500)
+        mockWebServer.enqueue(mockResponse)
+        onView(withId(R.id.tv_error))
+            .check(matches(isDisplayed()))
+        onView(withText("Oops.. something went wrong."))
+            .check(matches(isDisplayed()))
+    }
 }
